ac57062f63afaefad3c5ddb52ec40a1790a6e10e
Sabarithan P <sabarithanpalanivel@gmail.com>
2026-04-17 11:18:43 +0530

    fix case issue for navbar css

--- Frontend/smartcanteen-ui/src/components/Navbar.jsx
+++ Frontend/smartcanteen-ui/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
-import "./Navbar.css";
+import "./navbar.css";
 
 function Navbar() {
   const user = JSON.parse(sessionStorage.getItem("currentUser") || "{}");
