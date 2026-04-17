205cc2add25287558c49129531e985d0f713e549
Sabarithan P <sabarithanpalanivel@gmail.com>
2026-04-17 12:09:37 +0530

    updated backend url

--- Frontend/smartcanteen-ui/src/services/api.js
+++ Frontend/smartcanteen-ui/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/SmartCanteen/api";
+const BASE_URL = "https://smart-canteen-system-zjuv.onrender.com/api";
 
 /* =========================
    COMMON FETCH FUNCTION
